1508570465e57e6d7f8c93055ea8060e9f1662e7
Victor Héran <leroierrant@MacBook-Pro-de-Victor.local>
2023-02-17 19:29:32 +0100

    Request pars RequestLine and Header DOESNT WORK WITH BODY YET

--- includes/request.hpp
+++ includes/request.hpp
@@ -1,21 +1,65 @@
 #ifndef REQUEST_HPP
 # define REQUEST_HPP
 
-# include "includes.h"
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <cctype>
+#include <cstdlib>
+#include <vector>
+
+enum e_methods {
+	GET,
+	POST,
+	DELETE,
+	NUM_METHODS
+};
+
+enum {
+	METHOD,
+	URI,
+	PROTOCOL
+};
+
+enum {
+	MAJOR,
+	MINOR
+};
+
 class Request {
 	private:
-		std::string						_method;
-		std::string						_uri;
-		std::string						_protocol;
-		map<std::string, std::string>	_header;
+		static std::string	_valid_methods[];
 
 		void	parseRequestLine( std::string rLine );
-		Request( int fd );
+		void	parseHeader( std::string header);
+		bool	validMethod( void );
+		bool	validProtocol( void );
 	public:
+		std::string	request_line[3];
+		int			protocol[2];
+		std::map<std::string, std::string>	headers;
 
+		Request(const char* & buffer );
 		~Request( void );
 
+		const std::string	getHeaderValue( const std::string& key );
+		
+		class InvalidRequestLine: public std::exception {
+			virtual const char* what() const throw();
+		};
 
-}
+		class InvalidMethod: public std::exception {
+			virtual const char* what() const throw();
+		};
+
+		class InvalidURI: public std::exception {
+			virtual const char* what() const throw();
+		};
+
+		class InvalidProtocol: public std::exception {
+			virtual const char* what() const throw();
+		};
+
+};
 
 #endif
--- src_utils/request.cpp
+++ src_utils/request.cpp
@@ -1,9 +1,111 @@
 #include "includes.hpp"
 
+std::string	Request::_valid_methods[] = {"GET", "POST", "DELETE"};
 
-Request::Request( int fd ) {
-	//struct pollfd	fds = {.fd = fd, .events = POLLIN | POLLOUT};
+bool is_a_number(const std::string& s) {
+    
+	for (std::string::const_iterator it = s.begin(); it != s.end(); ++it) {
+        if (!isdigit(*it)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Request::Request( const char* & buffer ) {	
+	std::istringstream ss(buffer);
+	std::string token;
+
+	std::getline(ss, token);
+	this->parseRequestLine(token);
+
+	while(std::getline(ss, token)) {
+		this->parseHeader(token);
+	}
+}
+
+Request::~Request( void ) {}
+
+const char* Request::InvalidRequestLine::what() const throw( ){
+	return "Invalid Request Line";
+}
+
+const char* Request::InvalidMethod::what() const throw( ){
+	return "Invalid HTTP Method";
+}
+
+const char* Request::InvalidURI::what() const throw( ){
+	return "Invalid URI";
+}
+
+const char* Request::InvalidProtocol::what() const throw( ){
+	return "Invalid Protocol";
+}
+
+bool	Request::validMethod( void ) {
+	for (int i = 0; i < NUM_METHODS; i++) {
+		if (this->request_line[METHOD].compare(Request::_valid_methods[i]) == 0)
+			return true;
+	}
+	return false;
+}
+
+bool	Request::validProtocol( void ) {
+	std::istringstream ss(this->request_line[PROTOCOL]);
+	std::string token;
+
+	if (!std::getline(ss, token, '/'))
+		return false;
+	if (token.compare("HTTP"))
+		return false;
+	if (!std::getline(ss, token, '.'))
+		return false;
+	if (!is_a_number(token))
+		return false;
+	this->protocol[MAJOR] = std::atoi(token.data());
+	if (!std::getline(ss, token, '.'))
+		return false;
+	if (!is_a_number(token))
+		return false;
+	this->protocol[MINOR] = std::atoi(token.data());
+	return true;
+}
+
+void	Request::parseRequestLine( std::string rLine ) {
+	std::istringstream ss(rLine);
+	std::string token;
+
+	for (int i = METHOD; i <= PROTOCOL; i++) {
+		if (std::getline(ss, token, ' '))
+			this->request_line[i] = token;
+		else
+			throw Request::InvalidRequestLine();
+	}
+	if (std::getline (ss, token, ' '))
+		throw Request::InvalidRequestLine();
+	if (!this->validMethod())
+		throw Request::InvalidMethod();
+	if (this->request_line[URI].compare(0,1, "/"))
+		throw Request::InvalidURI();
+	if (!this->validProtocol())
+		throw Request::InvalidProtocol();
+}
+
+void	Request::parseHeader( std::string header ) {
+	std::istringstream ss(header);
+	std::string key;
+	std::string value;
 	
+	std::getline(ss, key, ':');
+	std::getline(ss, value);
+	value.erase(0,1);
+	this->headers[key] = value;
+}
 
-
+const std::string	Request::getHeaderValue( const std::string& key ) {
+	std::map<std::string, std::string>::iterator it = headers.find(key);
+	if (it != this->headers.end())
+		return it->second;
+	else
+		return "Unkown Key";
 }
