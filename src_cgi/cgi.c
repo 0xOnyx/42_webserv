87b30610006ed008f60a6647119b45dc28374597
= <=>
2023-02-24 23:30:27 +0100

    creat first cgi but I am tired, I will continue tomorrow.

--- src_cgi/cgi.c
+++ src_cgi/cgi.c
@@ -1,6 +1,21 @@
-#include  <stdio.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 int	main(int argc, char **argv)
 {
+	struct s
+	int	visits;
+	int	fd;
 
+
+	if ((fd = fd = open("number_of_visit.txt", O_RDWR)) < 0)
+		return (1);
+	if (fstat(fd, &stat) < 0)
+	{
+		close(fd);
+		return (2);
+	}
+	if (lseek(fd, 0, SEEK_SET) != 0)
+		return (3);
+	return (0);
 }
